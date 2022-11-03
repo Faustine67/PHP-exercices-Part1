d8b7b8807f1f88eb925a875c3af8083a45adc163
Faustine67 <michelfaustine@gmail.com>
2022-11-03 09:32:31 +0100

    New exercice 5+6

--- exo5.php
+++ exo5.php
@@ -1,7 +1,7 @@
 <h1>Exercice5</h1>
 
-<p>Soit le montant en francs : 100.</p>
-Soit 100 francs= 15.24 <br>
+<p>Soit le montant en francs : 100</p>
+Soit 100 francs= 15.24€ <br>
 Ecrire un algorithme permettant de convertir la valeur en franc en euros <br>
 La valeur doit etre arrondie à 2 decimales
 
@@ -9,3 +9,8 @@ La valeur doit etre arrondie à 2 decimales
 
 <?php
 
+$montantfranc = 100;
+$valeureuro = 0.1524;
+$conversion = $montantfranc * $valeureuro;
+$conversion = round ($conversion, 2);
+echo "Le valeur en euros de $montantfranc est de $conversion";
---
+++ exo6.php
@@ -0,0 +1,12 @@
+<h1>Exercice6</h1>
+
+<p>Soit le prix unitairede l'article: 9,99
+Soit la quantité : 5 <br>
+Soit le taux de TVA : 0,2 <br>
+Soit le  montant de la facture à regler de : 59,94 <br>
+Ecrire un algorithme permettant de calculer un montant de facture à regler <br>
+La valeur doit exprimée en decimale</p>
+
+<h2>Résultat</h2>
+
+<?php
