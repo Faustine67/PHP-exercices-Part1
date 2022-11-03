dedf479748c0b271c5de69029b183873ff095faa
Faustine67 <michelfaustine@gmail.com>
2022-11-03 09:47:40 +0100

    exo6+7

--- exo6.php
+++ exo6.php
@@ -1,6 +1,6 @@
 <h1>Exercice6</h1>
 
-<p>Soit le prix unitairede l'article: 9,99
+<p>Soit le prix unitaire de l'article: 9,99 <br>
 Soit la quantité : 5 <br>
 Soit le taux de TVA : 0,2 <br>
 Soit le  montant de la facture à regler de : 59,94 <br>
@@ -10,3 +10,15 @@ La valeur doit exprimée en decimale</p>
 <h2>Résultat</h2>
 
 <?php
+
+$prixunitaire = 9.99;
+$quantite = 5;
+$tva = 0.2;
+
+// (9,99+(0.2*9.99))*5
+
+$montanttotal = ($prixunitaire +($tva  *$prixunitaire)) * $quantite;
+
+$montanttotal = round ($montanttotal, 1);
+
+echo "Le montant total de la facture est de $montanttotal";
