0bdbe29d66c688388d6e2547f71b344b6b616027
Faustine67 <michelfaustine@gmail.com>
2022-11-03 11:26:36 +0100

    exo7

--- exo7.php
+++ exo7.php
@@ -6,9 +6,26 @@ Soit minime : entre 10 et 11 ans <br>
 Soit cadet: à partir de 12 ans <br>
 Si categorie pas gérée: le preciser <br>
 Ecrire un algorithme permettant de renvoyer la categorie d'un enfant en fonction de son age <br>
-La valeur doit exprimée en decimale</p>
 
 <h2>Résultat</h2>
 
 <?php
+
+$age = 10;
+
+
+if ($age == 6 || $age== 7) {
+    $resultat = "poussin";
+} elseif($age == 8 || $age == 9 ) {
+    $resultat = "pupille";
+}elseif($age == 10 || $age == 11) {
+	$resultat="minime";
+} elseif($age<=12 ) {
+	$resultat = "cadet";
+} elseif($age>6 ) {
+ 	$resultat = "pas gérée";
+}
+
+
+echo "L'enfant qui a $age ans appartient à la catégorie $resultat";
 
