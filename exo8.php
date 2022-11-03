c0cc4de5cba6047a9627c8bbc6f9faacc9a91140
Faustine67 <michelfaustine@gmail.com>
2022-11-03 11:54:10 +0100

    exo8

---
+++ exo8.php
@@ -0,0 +1,24 @@
+
+<h1>Exercice8</h1>
+
+<p> Ecrire un algorithme qui renvoie la table de multiplication d'un nombre passé en paramètre <br>
+Proposer 2 solutions avec 2 types de boucles <br>
+
+<h2>Résultat</h2>
+
+<?php
+
+$table=8;
+// BOUCLES (loop)
+// Afficher les chiffres de 1 à 10
+
+// FOR (pour)
+// $i++  -->   $i = $i + 1
+
+for($i = 1; $i <= 10; $i++) {
+    echo $i*$table." ";
+}
+
+
+
+
