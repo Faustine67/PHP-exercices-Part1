31bdb7759ce91febc799bf4747f471e2b71e0d2b
Faustine67 <michelfaustine@gmail.com>
2022-11-03 10:00:39 +0100

    new7

---
+++ exo7.php
@@ -0,0 +1,14 @@
+<h1>Exercice7</h1>
+
+<p>Soit poussin : entre 6 et 7 ans <br>
+Soit pupille entre 8 et 9 ans <br>
+Soit minime : entre 10 et 11 ans <br>
+Soit cadet: à partir de 12 ans <br>
+Si categorie pas gérée: le preciser <br>
+Ecrire un algorithme permettant de renvoyer la categorie d'un enfant en fonction de son age <br>
+La valeur doit exprimée en decimale</p>
+
+<h2>Résultat</h2>
+
+<?php
+
