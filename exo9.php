2d368a6b81b5b39774a691c413a68da180c0862c
Faustine67 <michelfaustine@gmail.com>
2022-11-04 14:57:15 +0100

    exo 9

---
+++ exo9.php
@@ -0,0 +1,25 @@
+
+<h1>Exercice9</h1>
+
+<p> Ecrire un algorithme qui permet de savoir si une personne est 
+    imposable en fonction de son âge et de son sexe <br>
+
+ <p>Si c'est une femme dont l'age est entre 18 et 35 alors imposable <br>
+Si c'est un homme de plus de 20 ans alors imposable <br>
+Sinon, non imposable <br>
+
+<h2>Résultat</h2>
+
+
+<?php
+
+$genre1 = "femme";
+$genre2 = "homme";
+$age = 23;
+
+if($femme || ($age == 18 || $age== 35) ) {
+} elseif ($homme || $age>20 ){
+    $resultat = "imposable" {
+} else{
+    $resultat = "non imposable";
+}
